e132fffab8863179655e0abc86a93d2fcfe65adb
chichhA <edouard.goffinet@outlook.com>
2024-08-17 00:14:01 +0200

    WIP

--- Assets/Scripts/PlayerControllerNetwork.cs
+++ Assets/Scripts/PlayerControllerNetwork.cs
@@ -1,37 +1,24 @@
 using UnityEngine;
+using Unity.Netcode;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : NetworkBehaviour
 {
-    public float moveSpeed = 5f;     // Speed of the player movement
-    public float jumpForce = 5f;     // Force applied when the player jumps
-    public LayerMask groundLayer;    // Layer that defines what is considered ground
-    public Transform groundCheck;    // A point used to check if the player is on the ground
-    public float groundCheckRadius = 0.3f; // Radius of the ground check
-
-    private Rigidbody rb;            // Reference to the player's Rigidbody
-    private bool isGrounded;         // Whether the player is on the ground or not
-
-    void Start()
+ 
+    private void Update()
     {
-        rb = GetComponent<Rigidbody>(); // Get the Rigidbody component attached to the player
+        if (!IsOwner) return;
+
+        Vector3 moveDir = new Vector3(0,0,0);
+
+        if (Input.GetKey(KeyCode.W)) moveDir.z += +1f;
+        if (Input.GetKey(KeyCode.S)) moveDir.z += -1f;
+        if (Input.GetKey(KeyCode.A)) moveDir.x += -1f;
+        if (Input.GetKey(KeyCode.D)) moveDir.x += +1f;
+
+        float moveSpeed = 3f;
+
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+
     }
-
-    void Update()
-    {
-        // Handle movement
-        float moveInputX = Input.GetAxis("Horizontal");
-        float moveInputZ = Input.GetAxis("Vertical");
-
-        Vector3 move = transform.right * moveInputX + transform.forward * moveInputZ;
-        rb.velocity = new Vector3(move.x * moveSpeed, rb.velocity.y, move.z * moveSpeed);
-
-        // Check if the player is on the ground
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundCheckRadius, groundLayer);
-
-        // Handle jumping
-        if (Input.GetButtonDown("Jump") && isGrounded)
-        {
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-        }
-    }
-}
+} 
