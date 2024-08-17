470ed3d274f4ccd3f3355ff7af80574a91171794
Etienne Hüsler <etienne.husler@he-arc.ch>
2024-08-17 15:17:13 +0200

    Raycast to get outline and size

---
+++ Assets/Scripts/Props/PropHighlight.cs
@@ -0,0 +1,71 @@
+using UnityEditor.SceneTemplate;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PropHighlight : MonoBehaviour
+{
+    public Camera playerCamera;
+    public float raycastDistance = 100f;
+    public float outlineWidth = 100f;
+    public Color outlineColor = Color.red;
+    public Text text;
+
+    private GameObject currentHighlightedObject;
+
+    void Update()
+    {
+        if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out RaycastHit hit, raycastDistance))
+        {
+            GameObject hitObject = hit.collider.gameObject;
+            
+            if (hitObject.TryGetComponent<Prop>(out var prop))
+            {
+                if (hitObject != currentHighlightedObject)
+                {
+                    RemoveHighlight();
+                    AddHighlight(hitObject);
+                    currentHighlightedObject = hitObject;
+                    text.text = hitObject.name + " [ Size: " + prop.GetSizeCategory() + "(" + prop.GetSize() + ") ]";
+                }
+            }
+            else
+            {
+                RemoveHighlight();
+                currentHighlightedObject = null;
+                text.text = "";
+            }
+        }
+        else
+        {
+            RemoveHighlight();
+            currentHighlightedObject = null;
+            text.text = "";
+        }
+    }
+
+    void AddHighlight(GameObject obj)
+    {
+        if (obj != null)
+        {
+            if (obj.GetComponent<Outline>() == null)
+            {
+                var outline = obj.AddComponent<Outline>();
+                outline.OutlineMode = Outline.Mode.OutlineAll;
+                outline.OutlineColor = outlineColor;
+                outline.OutlineWidth = outlineWidth;
+            }
+        }
+    }
+
+    void RemoveHighlight()
+    {
+        if (currentHighlightedObject != null)
+        {
+            Outline outline = currentHighlightedObject.GetComponent<Outline>();
+            if (outline != null)
+            {
+                Destroy(outline);
+            }
+        }
+    }
+}
