1141dc1a3f4e084b23500e9dfdeef21ecc7362be
Sarah Mayer <Sarah.Slash10@gmail.com>
2024-08-18 16:22:46 +0200

    Add ambiant music, shooting noise and random noise

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -9,6 +9,9 @@ public class Player : NetworkBehaviour
     [SerializeField] private GameObject meshParent;
     [SerializeField] private Transform objectGrabPoint;
     [SerializeField] private Camera playerCamera;
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip[] audioClips;
+    [SerializeField] private AudioClip shootingNoise;
 
     [Header("Parameters")]
     [SerializeField] private float playerHeight = 2f;
@@ -42,6 +45,11 @@ public class Player : NetworkBehaviour
         if (Input.GetKeyDown(KeyCode.Q))
         {
             Shoot();
+        }
+
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            PlayRandomSound();
         }
 
         HighlightProps();
@@ -150,6 +158,10 @@ public class Player : NetworkBehaviour
         }
 
         Debug.DrawRay(rayOrigin, ray * shootRaycastDistance, Color.red, 2f);
+
+        audioSource.clip = shootingNoise;
+        audioSource.Play();
+
     }
 
     private void HighlightProps()
@@ -206,5 +218,13 @@ public class Player : NetworkBehaviour
                 Destroy(outline);
             }
         }
+    }
+    
+    public void PlayRandomSound()
+    {
+        int randomIndex = Random.Range(0, audioClips.Length);
+        AudioClip randomClip = audioClips[randomIndex];
+        audioSource.clip = randomClip;
+        audioSource.Play();
     }
 }
